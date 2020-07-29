326d2d6230300dc0e04a4d4c296903a261fad638
Kevin Leung <kevinresol@gmail.com>
2020-07-29 15:21:00 +0800

    Add droppable

--- demo/Heaps.hx
+++ demo/Heaps.hx
@@ -22,10 +22,7 @@ class Heaps extends hxd.App {
 		manager.setBackend(backend);
 		
 		hxd.Window.getInstance().useScreenPixels = false;
-		coconut.ui.Renderer.mount(
-			s2d,
-			'<Dummy manager=${manager}/>'
-		);
+		coconut.ui.Renderer.mount(s2d, '<Dummy manager=${manager}/>');
 	}
 	static function main() {
 		new Heaps();
@@ -43,36 +40,75 @@ typedef MyResult = {
 
 class Dummy extends coconut.h2d.View {
 	@:attr var manager:Manager<MyItem, MyResult, PureInteractive>;
-	@:computed var model:DraggableModel<MyItem, MyResult, {final x:Int; final y:Int; final item:MyItem;}, PureInteractive> = {
+	@:computed var draggable:DraggableModel<MyItem, MyResult, {final x:Int; final y:Int; final item:MyItem;}, PureInteractive> = {
 		var last = tink.s2d.Point.xy(0, 0);
+		trace('new draggable');
 		new DraggableModel({
 			type: 'FOO', 
 			manager: manager, 
 			canDrag: () -> true,
 			onDragStart: () -> {foo: 'bar'},
-			onDragEnd: ctx -> last = ctx.getSourceClientOffset(),
+			onDragEnd: ctx -> {
+				trace(ctx.getDropResult());
+				switch ctx.getSourceClientOffset() {
+					case null: // skip
+					case v: last = v;
+				}
+			},
 			isDragging: item -> item != null,
 			collect: ctx -> {
 				final pos = switch ctx.getSourceClientOffset() {
 					case null: last;
-					case v: v;
+					case v: last = v;
 				}
 				{x: Std.int(pos.x), y: Std.int(pos.y), item: ctx.getItem()}
 			}
 		});
 	}
+	@:computed var droppable:DroppableModel<MyItem, MyResult, {final isOver:Bool;}, PureInteractive> = {
+		var model:DroppableModel<MyItem, MyResult, {final isOver:Bool;}, PureInteractive> = null;
+		model = new DroppableModel({
+			type: 'FOO', 
+			manager: manager, 
+			canDrop: item -> true,
+			onHover: item -> trace('hover'),
+			onDrop: item -> {
+				trace('drop');
+				{bar: 'baz'}
+			},
+			collect: ctx -> {
+				{isOver: ctx.getTargetIds().contains(model.targetId) && ctx.canDropOnTarget(model.targetId)}
+			}
+		});
+		model;
+	}
 		
 		
 	function render() '
+		<>
+		<Droppable
+			model=${droppable}
+			renderChildren=${(ref, attrs) -> (
+				<Interactive x=${100} y=${100} width=${100} height=${20} backgroundColor=${attrs.isOver ? 0xFFCCCCCC : 0xFF2222FF} onClick=${trace('clicked')} ref=${ref}>
+					<Text
+						x=${50} 
+						y=${2} 
+						font=${hxd.res.DefaultFont.get()} 
+						text="Target"
+						textAlign=${Center}
+						textColor=${0}
+					/>
+				</Interactive>
+			)}
+		/>
 		<Draggable
-			model=${model}
+			model=${draggable}
 			renderChildren=${(ref, attrs) -> (
 				<Interactive x=${attrs.x} y=${attrs.y} width=${100} height=${20} backgroundColor=${attrs.item == null ? 0xFFCCCCCC : 0xFFFFFF22} onClick=${trace('clicked')} ref=${ref}>
 					<Text
 						x=${50} 
 						y=${2} 
 						font=${hxd.res.DefaultFont.get()} 
-						// text=${model.sourceId + ':' + model.type + ':' + (model.connection == null) + ':' + model.manager.getMonitor().isDragging()}
 						text=${'Dragging: ' + (attrs.item == null ? 'false' : attrs.item.foo)}
 						textAlign=${Center}
 						textColor=${0}
@@ -80,10 +116,14 @@ class Dummy extends coconut.h2d.View {
 				</Interactive>
 			)}
 		/>
+		
+		
+		</>
 	';
 	
 	override function viewWillUnmount() {
-		model.dispose();
+		draggable.dispose();
+		droppable.dispose();
 	}
 }
 
---
+++ src/coconut/dragdrop/DroppableModel.hx
@@ -0,0 +1,71 @@
+package coconut.dragdrop;
+
+import why.dragdrop.*;
+using tink.CoreApi;
+
+class DroppableModel<Item, Result, Attrs, Node> implements coconut.data.Model {
+	@:constant var type:String;
+	@:constant var manager:Manager<Item, Result, Node>;
+	@:constant var canDrop:Item->Bool;
+	@:constant var onHover:Item->Void;
+	@:constant var onDrop:Item->Result;
+	@:constant var collect:Context<Item, Result>->Attrs;
+	
+	@:editable private var node:Node = null;
+	
+	@:computed var target:Target<Item, Result> = new Target({
+		canDrop: (ctx, id) -> canDrop(ctx.getItem()),
+		hover: (ctx, id) -> onHover(ctx.getItem()),
+		drop: (ctx, id) -> onDrop(ctx.getItem()),
+	});
+	
+	@:skipCheck @:computed var registry:Registry<Item, Result> = manager.getRegistry();
+	@:computed var targetId:TargetId = {
+		switch ($last) {
+			case Some(id): registry.removeTarget(id);
+			case None:
+		}
+		registry.addTarget([type], target);
+	}
+	@:computed var connection:CallbackLink = {
+		$last.orNull().cancel();
+		if(node == null) null;
+		else manager.getBackend().connectDropTarget(targetId, node, {});
+	}
+	@:computed var attrs:Attrs = {
+		connection; // HACK: make it tracked
+		collect == null ? null : collect(manager.getMonitor());
+	}
+	
+	public function ref(node) 
+		this.node = node;
+	
+	public function dispose() {
+		connection.cancel();
+		registry.removeTarget(targetId);
+	}
+}
+
+@:structInit
+private class Target<Item, Result> implements DropTarget<Item, Result> {
+	final _canDrop:(ctx:Context<Item, Result>, targetId:TargetId)->Bool;
+	final _hover:(ctx:Context<Item, Result>, targetId:TargetId)->Void;
+	final _drop:(ctx:Context<Item, Result>, targetId:TargetId)->Result;
+	
+	public inline function new(o) {
+		_canDrop = o.canDrop;
+		_hover = o.hover;
+		_drop = o.drop;
+	}
+	
+	
+	public function canDrop(ctx:Context<Item, Result>, targetId:TargetId):Bool {
+		return _canDrop(ctx, targetId);
+	}
+	public function hover(ctx:Context<Item, Result>, targetId:TargetId):Void {
+		_hover(ctx, targetId);
+	}
+	public function drop(ctx:Context<Item, Result>, targetId:TargetId):Result {
+		return _drop(ctx, targetId);
+	}
+}
